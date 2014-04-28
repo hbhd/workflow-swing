e00b56fd24d098183be654b6371a358f14d377a7
zhouwd <zhouwendong006@gmail.com>
2014-04-28 10:46:58 +0800

    this  for test.

---
+++ test/src/main/java/TestA.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Joe
+ * Date: 14-4-28
+ * Time: 上午10:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestA {
+
+    public static void main(){
+        System.out.println("ETDE");
+    }
+}
